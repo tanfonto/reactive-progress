481a295b52a2a27641e4f530ab0c9e571bf0670f
whapky <sieradzpf@gmail.com>
2018-03-24 08:38:11 +0100

    added bundle minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,15 @@
 import commonjs from 'rollup-plugin-commonjs'
 import nodeResolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
-import { into, map } from 'ramda'
+import uglify from 'rollup-plugin-uglify'
+import { defaultTo, union, flatten, into, map, length, zipWith, repeat, always } from 'ramda'
 
-const MODULE_NAME = 'MProgress'
+const MODULE_NAME = always('MProgress')
+const minSuffix = always('.min')
 const formats = [ 'cjs', 'umd', 'es' ]
 
-const produceConfig = format => {
-  return {
-    input: 'src/main.js',
-    output: {
-      format: [ format ],
-      file: `dist/mprogress.${format}.js`,
-    name: MODULE_NAME
-  },
-  plugins: [
+const produceConfig = ({ format, suffix }) => {
+  const plugins = union([
     babel({
         exclude: 'node_modules/**'
     }),
@@ -27,11 +22,25 @@ const produceConfig = format => {
       include: [ 'src/**', 'node_modules/**' ],
       exclude: ['src/**/*.test.js', 'src/tester.js'],
       sourceMap: false
-    })
-  ]
+    })],  
+    [...(suffix ? [uglify()] : [])]
+  )
+
+  return {
+    input: 'src/main.js',
+    output: {
+      format: [ format ],
+      file: `dist/mprogress.${format}${defaultTo('', suffix)}.js`,
+    name: MODULE_NAME()
+  },
+  plugins: plugins
   }
 }
 
-const configs = into([], map(produceConfig), formats)
+const configs = into(
+  [], 
+  map(produceConfig), 
+  flatten([...zipWith((format, suffix) => [{ format }, { format, suffix }], formats, repeat(minSuffix(), length(formats)))])
+)
 
 export default configs
